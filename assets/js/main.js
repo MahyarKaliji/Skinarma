4b1637dd73f474d4d623fb79525243b9a5802aae
MahyarKaliji <mahyar.kaliji@gmail.com>
2024-07-05 11:51:10 +0330

    register and login added

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,28 +7,43 @@ function searchBox() {
   }
 }
 
+function resetPassword(){
+  const login = document.getElementById("login");
+  const reset = document.getElementById("reset");
+  login.style.display='none';
+  reset.style.display='block';
+}
+
+function login(){
+  const login = document.getElementById("login");
+  const reset = document.getElementById("reset");
+  reset.style.display='none';
+  login.style.display='block';
+}
+
+
 // Category product script
-window.onload = function () {
-  const anchorTags = document.querySelectorAll("a");
-  anchorTags.forEach(function (a) {
-    a.addEventListener("click", function (ev) {
-      ev.preventDefault();
-    });
-  });
-  const dropdownEl = document.querySelector("[data-dropdown-toggle]");
-  if (dropdownEl) {
-    dropdownEl.click();
-  }
-  const modalEl = document.querySelector("[data-modal-toggle]");
-  if (modalEl) {
-    modalEl.click();
-  }
-  const dateRangePickerEl = document.querySelector("[data-rangepicker] input");
-  if (dateRangePickerEl) {
-    dateRangePickerEl.focus();
-  }
-  const drawerEl = document.querySelector("[data-drawer-show]");
-  if (drawerEl) {
-    drawerEl.click();
-  }
-};
+// window.onload = function () {
+//   const anchorTags = document.querySelectorAll("a");
+//   anchorTags.forEach(function (a) {
+//     a.addEventListener("click", function (ev) {
+//       ev.preventDefault();
+//     });
+//   });
+//   const dropdownEl = document.querySelector("[data-dropdown-toggle]");
+//   if (dropdownEl) {
+//     dropdownEl.click();
+//   }
+//   const modalEl = document.querySelector("[data-modal-toggle]");
+//   if (modalEl) {
+//     modalEl.click();
+//   }
+//   const dateRangePickerEl = document.querySelector("[data-rangepicker] input");
+//   if (dateRangePickerEl) {
+//     dateRangePickerEl.focus();
+//   }
+//   const drawerEl = document.querySelector("[data-drawer-show]");
+//   if (drawerEl) {
+//     drawerEl.click();
+//   }
+// };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,23 @@ module.exports = {
   content: ["./pages/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage:{// background image
-        'header-image': "url('/assets/images/header/header-background.jpg')",
-        // 'header-bg': "url('/assets/images/header/bg.jpg')"
+      backgroundImage: {
+        'header-bg': "url('G:/Learning/06-Web%20Designing%20-%20Class/01-Exams/14030403/assets/images/02-header/header-background.png')",
+        'main-bg': "url('G:/Learning/06-Web%20Designing%20-%20Class/01-Exams/14030403/assets/images/03-main/main-bg.png')",
+        'footer-bg': "url('G:/Learning/06-Web Designing - Class/01-Exams/14030403/assets/images/06-footer/bg2.jpg')"
       },
-      gridTemplateColumns:{ //navbar layout
-        'navbar-layout': '2fr,6fr,4fr',
-        'product-layout': '4fr,1fr'
+      gridTemplateColumns: {
+        //navbar layout
+        "navbar-layout": "2fr,6fr,4fr",
+        "product-layout": "4fr,1fr",
       },
-      fontSize:{
-        'main-title': '13rem'
+      fontSize: {
+        "main-title": "13rem",
       },
-      fontFamily:{
-        body:['Mukta']
-      }
+      fontFamily: {
+        body: ["Mukta"],
+      },
     },
   },
   plugins: [],
-}
+};
